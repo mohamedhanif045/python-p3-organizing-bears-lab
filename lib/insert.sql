73fe888071d13c8917429cdd5edfd5cc9fcd0002
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-08-30 07:39:12 +0300

    Fix SQL query for selecting the youngest bear and returning its name and age

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+       ('Rowdy', 5, 'M', 'Black', 'Aggressive', 1),
+       ('Tabitha', 7, 'F', 'Brown', 'Friendly', 1),
+       ('Sergeant Brown', 12, 'M', 'Brown', 'Stern', 1),
+       ('Melissa', 4, 'F', 'Black', 'Playful', 1),
+       ('Grinch', 11, 'M', 'Green', 'Grumpy', 1),
+       ('Wendy', 3, 'F', 'Brown', 'Curious', 1),
+       (NULL, 2, 'F', 'White', 'Shy', 1);
